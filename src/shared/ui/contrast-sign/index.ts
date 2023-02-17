6bfb66a230eb68e3d1551ef0ab08e842035e81b9
starwatcher <redmoonloon@outlook.com>
2023-02-17 23:28:18 +1000

    add contrast-sign component add import of paragrapg in public api of shared/ui

---
+++ src/shared/ui/contrast-sign/index.ts
@@ -0,0 +1,1 @@
+export { ContrastSign } from './contrast-sign'
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,1 +1,4 @@
 export { Button } from './button'
+export { Image } from './image'
+export { Paragraph } from './paragraph'
+export { ContrastSign } from './contrast-sign'
