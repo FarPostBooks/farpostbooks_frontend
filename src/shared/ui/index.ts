cecf1b74eed4a75d16ee6a4dfcec02b5d8c9bc95
starwatcher <redmoonloon@outlook.com>
2023-02-21 17:28:27 +1000

    add form atom

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,6 +4,7 @@ export { Input } from './input'
 export { Paragraph } from './paragraph'
 export { ContrastSign } from './contrast-sign'
 export { Header } from './header'
+export { Form } from './form'
 
 export type { TelegramLoginWidgetData } from './telegram-login-widget'
 export { TelegramLoginWidget } from './telegram-login-widget'
